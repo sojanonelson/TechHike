96780bf1da1e8651dba8e16fb9d79e2fb99b3fad
sojan soj <sojanonelson>
2025-04-05 22:18:37 +0530

    TaDa

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './app';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { Analytics } from "@vercel/analytics/react"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Analytics/>
     <App />
 
     </Provider>
